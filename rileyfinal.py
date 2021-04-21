7a9f05b310eeb12babf4ac1321275a34ee5ffe51
DrinkElixr <75812941+DrinkElixr@users.noreply.github.com>
2021-04-21 11:25:52 -0400

    Update rileyfinal.py

--- rileyfinal.py
+++ rileyfinal.py
@@ -21,22 +21,26 @@ def chart_options(index):
 
     return color,alpha,ascending,filter_by,keep
 def filter_by_state(use,index):
-    df=load_data(True)
-    states=pd.unique(df["STATE"])
-    d1=[]
-    for state in states:
-        d2=[]
-        count=len(df[df["STATE"]==state])
+    try:
+        dfn=pd.read_csv("FilterbyState.csv")
+    except:
+        df=load_data(True)
+        states=pd.unique(df["STATE"])
+        d1=[]
+        for state in states:
+            d2=[]
+            count=len(df[df["STATE"]==state])
 
-        d2.append(state)
-        d2.append(count)
-        d2.append(df[df["STATE"]==state]["LON"].mean())
-        d2.append(df[df["STATE"]==state]["LAT"].mean())
-        d1.append(d2)
-    dfn=pd.DataFrame(d1)
-    dfn.columns=["state","number_of_schools","lon","lat"]
-
+            d2.append(state)
+            d2.append(count)
+            d2.append(df[df["STATE"]==state]["LON"].mean())
+            d2.append(df[df["STATE"]==state]["LAT"].mean())
+            d1.append(d2)
+        dfn=pd.DataFrame(d1)
+        dfn.columns=["state","number_of_schools","lon","lat"]
+        dfn.to_csv("FilterbyState.csv")
     if use=="Maps":
+        st.header("Map of Schools by State")
         view=pdk.ViewState(latitude=dfn["lat"].mean(),longitude=dfn["lon"].mean(),pitch=20,zoom=5)
         map = pdk.Layer("ColumnLayer",data=dfn,get_position=["lon", "lat"],get_elevation="number_of_schools",elevation_scale=1000,radius=10000,pickable=True,auto_highlight=True,get_fill_color=[225,225,225,225])
         tooltip = {"html": "<b>{state}</b>: {number_of_schools} schools","style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"}}
@@ -63,24 +67,29 @@ def filter_by_state(use,index):
         st.pyplot(plt)
         plt.clf()
 def filter_by_town(use,index):
-    df=load_data(True)
-    df["st"]=df["STATE"]+","+df["CITY"]
+    try:
+        dfn=pd.read_csv("FilterbyTown.csv")
+    except:
+        df=load_data(True)
+        df["st"]=df["STATE"]+","+df["CITY"]
 
-    states=pd.unique(df["st"])
-    d1=[]
-    for state in states:
-        d2=[]
-        count=len(df[df["st"]==state])
+        states=pd.unique(df["st"])
+        d1=[]
+        for state in states:
+            d2=[]
+            count=len(df[df["st"]==state])
 
-        d2.append(state)
-        d2.append(count)
-        d2.append(df[df["st"]==state]["LON"].mean())
-        d2.append(df[df["st"]==state]["LAT"].mean())
-        d1.append(d2)
-    dfn=pd.DataFrame(d1)
-    dfn.columns=["town","number_of_schools","lon","lat"]
+            d2.append(state)
+            d2.append(count)
+            d2.append(df[df["st"]==state]["LON"].mean())
+            d2.append(df[df["st"]==state]["LAT"].mean())
+            d1.append(d2)
+        dfn=pd.DataFrame(d1)
+        dfn.columns=["town","number_of_schools","lon","lat"]
+        dfn.to_csv("FilterbyTown.csv")
 
     if use=="Maps":
+        st.header("Map of Schools by Town")
         view=pdk.ViewState(latitude=dfn["lat"].mean(),longitude=dfn["lon"].mean(),pitch=20,zoom=5)
         map = pdk.Layer("ColumnLayer",data=dfn,get_position=["lon", "lat"],get_elevation="number_of_schools",elevation_scale=1000,radius=1000,pickable=True,auto_highlight=True,get_fill_color=[225,225,225,225])
         tooltip = {"html": "<b>{town}</b>: {number_of_schools} schools","style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"}}
@@ -107,8 +116,10 @@ def filter_by_town(use,index):
         st.pyplot(plt)
         plt.clf()
 def all_colleges(use,index):
+
     df=load_data(True)
     if use=="Maps":
+        st.header("Map of all Schools")
         view=pdk.ViewState(latitude=df["LAT"].mean(),longitude=df["LON"].mean(),pitch=20,zoom=5)
         map = pdk.Layer("ColumnLayer",data=df,get_position=["LON", "LAT"],get_elevation=100,elevation_scale=1000,radius=500,pickable=True,auto_highlight=True,get_fill_color=[225,225,225,225])
         tooltip = {"html": "<b>{NAME}</b>: {STREET}, {CITY} {STATE}","style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"}}
@@ -118,9 +129,9 @@ def all_colleges(use,index):
 
 def main():
     st.title("College Explorer")
-    mm=st.sidebar.selectbox("What do you want to see?",["Maps","Charts","Both"])
+    mm=st.sidebar.radio("What do you want to see?",["Maps","Charts"])
     if mm=="Maps":
-        sm=st.multiselect("Which maps would you like to see?",["Colleges by State","All Colleges","Colleges by Town"])
+        sm=st.multiselect("Which maps would you like to see? (can select more than 1)",["Colleges by State","All Colleges","Colleges by Town"])
         for i,x in enumerate(sm):
             if x=="Colleges by State":
                 filter_by_state(mm,i)
@@ -129,7 +140,7 @@ def main():
             if x=="Colleges by Town":
                 filter_by_town(mm,i)
     if mm=="Charts":
-        sm=st.multiselect("Which maps would you like to see?",["Colleges by State","All Colleges","Colleges by Town"])
+        sm=st.multiselect("Which Chart would you like to see? (can select more than 1)",["Colleges by State","Colleges by Town"])
         for i,x in enumerate(sm):
             if x=="Colleges by State":
                 filter_by_state(mm,i)
@@ -139,3 +150,4 @@ def main():
 
 
 main()
+
